f4af61d9b332bf226d0556e557fad8b62b788326
Yoav Ziv <yoav.ziv02@post.runi.ac.il>
2025-11-06 17:34:51 +0200

    100%

--- NumWords.java
+++ NumWords.java
@@ -2,13 +2,13 @@
 
 public class NumWords {
 	public static void main(String args[]) {
-	    int units =0; 
-        int tens =0;
-        int hundreds =0;
+	    int units = 0; 
+        int tens = 0;
+        int hundreds = 0;
         int number = Integer.parseInt(args[0]);
         boolean isDone = false;
         
-        if(number > 0 && number <= 99 && isDone == false)
+        if (number > 0 && number <= 99 && isDone == false)
         {
             units = number % 10;
             tens = number / 10;
@@ -21,22 +21,13 @@ public class NumWords {
             isDone = true;
         }
         
-        
-        while (number > 0 && isDone == false)
+        if (number >= 100 && isDone == false)
         {
-            if(number > 99 && number <= 999)
-            {
-                units = number % 10;
-            }
-            if(number <= 99 && number > 10)
-            {
-                tens = number % 10;
-            }
-            if(number > 0 && number < 10)
-            {
-                hundreds = number % 10;
-            }
+            units = number % 10;
             number = number / 10;
+            tens = number % 10;
+            hundreds = number / 10;
+            isDone = true;
         }
         System.out.println(hundreds + " hundreds, " + tens + " tens, and " + units + " ones.");
 	}
